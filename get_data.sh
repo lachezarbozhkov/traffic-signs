b4fae3c1c965a403ec5cc001ac853730d2339b6f
Lachezar Bozhkov <lachezar.bozhkov@gmail.com>
2016-11-26 18:02:56 +0000

    Add get data script and initial Keras notebook

---
+++ get_data.sh
@@ -0,0 +1,4 @@
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580ad790_traffic-sign-classification-with-keras/traffic-sign-classification-with-keras.ipynb
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580ad86d_train/train.p
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580ad89e_test/test.p
+
